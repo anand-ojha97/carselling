1dbac61164a7fc1f517ef4bf666f8acdc9df0d45
anand-ojha97 <anandojha1432@gmail.com>
2024-01-10 12:03:55 +0530

    car-list-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Register from "./buyer-flow/components/Auth/Register";
 import "font-awesome/css/font-awesome.min.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landingpage from "./buyer-flow/components/Landing-page/Landingpage";
+import CarList from "./buyer-flow/components/Car-listing/CarList";
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<Landingpage />}  />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />}  />
-        <Route path="*" element={<Landingpage />}  />
+        <Route path="/car-list" element={<CarList />}  />``
       </Routes>
     </Router>
   );
